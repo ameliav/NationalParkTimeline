016bf306cd7c11cf325eafd278a7d5e1f4c75384
Amelia V <amelianvance@gmail.com>
2020-07-14 20:30:25 -0400

    timeline as main page

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,27 @@
 import React from 'react';
-import { Router, Route } from 'react-router-dom';
+//import { HashRouter as Router, Route } from 'react-router-dom';
 import Timeline from './Timeline';
-import Login from './Login';
-import history from "../history";
+//import Login from './Login';
+//import history from "../history";
 
 class App extends React.Component {
  
   render() {
     return (
       <div>
-        <Router history={history}>
-          <div>            
-            <Route path="/" exact component={Login} />
-            <Route path="/timeline" component={Timeline} />
-          </div>
-        </Router>
+        <Timeline/>
       </div>
     )
   }
 }
 
 export default App;
+
+/*
+<Router history={history} basename="/">
+          <div>
+            <Route path="/" exact component={Login} />
+            <Route path="/#/timeline" component={Timeline} />
+          </div>
+        </Router>
+*/
